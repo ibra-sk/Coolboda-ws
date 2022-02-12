183563af1452b43737ca948c20812417a979f6af
Ibrahim Saleh <76790442+ibra-kekia@users.noreply.github.com>
2022-02-12 10:07:25 +0300

    Another changes

--- index.js
+++ index.js
@@ -28,6 +28,7 @@ var server = app.listen(port, function(){
     console.log(`listining request to port ${port}`);
 });
 
+
 //Route Setup
 app.get("/", (req, res) => {
     res.send('hello world');
@@ -57,3 +58,5 @@ io.on('connection', (socket) => {
     console.log('Client connected');
     socket.on('disconnect', () => console.log('Client disconnected'));
 });
+
+setInterval(() => io.emit('time', new Date().toTimeString()), 1000);
